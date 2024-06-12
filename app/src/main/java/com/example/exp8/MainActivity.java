df0655b92132e8544e96afbe1c2f4e7d46cbde42
gprecnotes <gprecnotes@gmail.com>
2024-06-12 14:11:49 +0530

    first commit

---
+++ app/src/main/java/com/example/exp8/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.exp8;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioGroup radioGroup;
+    Button submit, clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submit = findViewById(R.id.submit);
+        clear = findViewById(R.id.clear);
+        radioGroup = findViewById(R.id.groupradio);
+
+        radioGroup.clearCheck();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                if (selectedId == -1) {
+                    Toast.makeText(MainActivity.this, "No answer has been selected", Toast.LENGTH_SHORT).show();
+                } else {
+                    RadioButton radioButton = (RadioButton) radioGroup.findViewById(selectedId);
+                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                radioGroup.clearCheck();
+            }
+        });
+    }
+}
